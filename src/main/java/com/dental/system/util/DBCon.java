69fab2a42a93a741f3f502616f21238d9bc29cf7
Dinindu Senanayake <dinindusenanayake0@users.noreply.github.com>
2026-07-23 13:51:32 +0530

    Create database connection class

--- src/main/java/com/dental/system/util/DBCon.java
+++ src/main/java/com/dental/system/util/DBCon.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBCon {
+    //Database Details
     private static final String URL = "jdbc:mysql://localhost:3306/sunrise_dental_db";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
