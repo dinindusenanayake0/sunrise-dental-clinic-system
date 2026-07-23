036d028f15701994b9ca846e2a8784945bd6a319
Dinindu Senanayake <dinindusenanayake0@users.noreply.github.com>
2026-07-23 13:15:34 +0530

    Initial project setup

---
+++ src/main/java/com/dental/system/util/DBCon.java
@@ -0,0 +1,31 @@
+package com.dental.system.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBCon {
+    private static final String URL = "jdbc:mysql://localhost:3306/sunrise_dental_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection(){
+        Connection connection = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            System.out.println("Database Connected");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL Driver Not Found");
+            e.printStackTrace();
+
+        }catch (SQLException e){
+            System.out.println("Database Connection Failed");
+            e.printStackTrace();
+
+        }
+        return connection;
+    }
+}
